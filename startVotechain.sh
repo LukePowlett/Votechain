85e095cd4b7228a811ed893757a87fa7aae6d159
Luke Powlett <lukepowlett@live.co.uk>
2018-04-05 18:39:55 +0100

    ballot used default false

--- startVotechain.sh
+++ startVotechain.sh
@@ -1,3 +1,5 @@
 export FABRIC_VERSION="hlfv11"
-../fabric-tools/startFabric.sh
+startFabric.sh
+composer network install --archiveFile votechain-network@0.0.1.bna --card PeerAdmin@hlfv1
+composer network start --networkName votechain-network --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
 composer-rest-server -c admin@votechain-network -n never -w true
